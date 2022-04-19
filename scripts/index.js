2e85a86e96efc124089da4eb34c71d803f7861fa
Stas Chernov <schernovmail@yandex.ru>
2022-04-19 19:11:54 +0300

    Проектная работа 4 версия 3

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const closeProfileBtn = profilePopup.querySelector('.popup__close');
 const submitProfile = profilePopup.querySelector('.popup__edit-profile');
 const profileFullName = document.querySelector('.profile__full-name');
 const profileAbout = document.querySelector('.profile__about');
-const profilePopupFullName = document.querySelector('.popup__input-string_type_full-name');
-const profilePopupAbout = document.querySelector('.popup__input-string_type_about');
+const profilePopupFullName = document.querySelector('.popup__input_type_full-name');
+const profilePopupAbout = document.querySelector('.popup__input_type_about');
 
 function profilePopupOpen() {
   profilePopup.classList.add('popup_opened');
